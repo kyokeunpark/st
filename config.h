4195c6437468432cf88c2abbafcd38aee2d2f3d3
KyoKeun Park <kyokeunpark@gmail.com>
2019-12-29 15:49:11 -0500

    Added transparency

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-unsigned int alpha = 0xff;
+unsigned int alpha = 0xd0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
